30a7726c17694b01660a4e3c68129d1a87da7c33
dmslade <97462294+dmslade@users.noreply.github.com>
2022-04-07 15:09:30 -0500

    Add files via upload

---
+++ ClientSide/Module6/Module6Ex/Checkbox2.js
@@ -0,0 +1,53 @@
+function graphicBox(box) {
+    // be unobtrusive
+    if (!document.getElementById) return;
+ 
+    // find the object and its parent
+    obj = document.getElementById(box);
+    //parentobj = obj.parentNode;
+ 
+    // hide the regular checkbox
+    obj.style.display = "none";
+ 
+    // create the image element and set its onclick event
+    img = document.createElement("img");
+    img.addEventListener("click", Toggle);
+    img.src = "images/unchecked.png";
+ 
+    // save the checkbox id within the image ID
+    img.id = "img" + box;
+ 
+    // display the graphic checkbox
+    parentobj.insertBefore(img,obj);
+ }
+ 
+ function Toggle(e) {
+    if (!e) var e=window.event;
+ 
+    // find the image ID
+    img = (e.target) ? e.target : e.srcElement;
+ 
+    // find the checkbox by removing "img" from the image ID
+    checkid = img.id.substring(3);
+    checkbox = document.getElementById(checkid);
+ 
+    // "click" the checkbox
+    checkbox.click();
+ 
+    // display the right image for the clicked or unclicked state
+    if (checkbox.checked) {
+       file = "image/checked.png";
+    }  else {
+       file="image/unchecked.png";
+    }
+    img.src=file;
+ }
+ 
+ //replace the second checkbox with a graphic
+ graphicBox("graphic");
+ //graphicBox("check3");
+ //graphicBox("check4");
+//collection =[];
+collection = document.getElementsByClassName("graphic");
+// console.log("Value of collectoin" + collection);
+ 
